38d3c3b996bb9a25595c11f27c6eaa61f95c5b04
Jake Swogger <jakeswogger1@gmail.com>
2019-09-19 20:30:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  if array.include?(cheese_types) = true 
+  cheese_types = ["cheddar", "gouda", "camembert"]
+   if array.include?(cheese_types) = true 
     
   end 
   cheese_types = ["cheddar", "gouda", "camembert"]
